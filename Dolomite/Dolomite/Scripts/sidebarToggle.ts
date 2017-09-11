3c2ad33db9b5185758c6db3a6376031ec939f6aa
Malakahh <malakahh@users.noreply.github.com>
2017-09-11 22:21:02 +0200

    w

--- Dolomite/Dolomite/Scripts/sidebarToggle.ts
+++ Dolomite/Dolomite/Scripts/sidebarToggle.ts
@@ -1,1 +1,21 @@
-﻿
+﻿let toggled : boolean = false
+
+$(document).ready(function() {
+
+
+	$(".dSidebarToggler").click(function(e) {
+		e.preventDefault();
+		if (!toggled) {
+			$(".dSidebar").addClass("col-md-12");
+			$(".dSidebar").removeClass("col-md-10");
+			toggled = true
+		} else {
+			$(".dSidebar").addClass("col-md-10");
+			$(".dSidebar").removeClass("col-md-12");
+			toggled = false
+		}
+
+	
+	})
+
+})
