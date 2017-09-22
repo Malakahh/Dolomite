affcfb62ffd52852e60fa112e350468ae155b10c
Malakahh <malakahh@users.noreply.github.com>
2017-09-23 01:57:36 +0200

    Better sidebar toggler

--- Dolomite/Dolomite/wwwroot/ts/sidebar.js
+++ Dolomite/Dolomite/wwwroot/ts/sidebar.js
@@ -1,15 +1,19 @@
 var sidebarHidden = true;
 var dSidebar = document.getElementsByClassName("dSidebar")[0];
 var dMainContent = document.getElementsByClassName("dMainContent")[0];
+var dSidebarHamburger = document.getElementsByClassName("dSidebarHamburger")[0];
 document.getElementsByClassName("dSidebarToggler")[0].addEventListener("click", function () {
     if (sidebarHidden) {
         dSidebar.classList.add("dSidebarToggled");
         dMainContent.classList.add("dMainContentToggled");
+        dSidebarHamburger.classList.add("open");
         sidebarHidden = false;
     }
     else {
         dSidebar.classList.remove("dSidebarToggled");
         dMainContent.classList.remove("dMainContentToggled");
+        dSidebarHamburger.classList.remove("open");
         sidebarHidden = true;
     }
 });
+//# sourceMappingURL=sidebar.js.map
--- Dolomite/Dolomite/wwwroot/ts/sidebar.ts
+++ Dolomite/Dolomite/wwwroot/ts/sidebar.ts
@@ -1,17 +1,20 @@
 ﻿let sidebarHidden = true;
 const dSidebar = document.getElementsByClassName("dSidebar")[0];
 const dMainContent = document.getElementsByClassName("dMainContent")[0];
+const dSidebarHamburger = document.getElementsByClassName("dSidebarHamburger")[0];
 
 document.getElementsByClassName("dSidebarToggler")[0].addEventListener("click", function () {
     if (sidebarHidden) {
         dSidebar.classList.add("dSidebarToggled");
         dMainContent.classList.add("dMainContentToggled");
+        dSidebarHamburger.classList.add("open");
 
         sidebarHidden = false;
     } else {
         dSidebar.classList.remove("dSidebarToggled");
         dMainContent.classList.remove("dMainContentToggled");
-
+        dSidebarHamburger.classList.remove("open");
+        
         sidebarHidden = true;
     }
 });
