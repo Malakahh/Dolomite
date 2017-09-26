3d909e1def530c179b021cf55e5e73d13bb39021
Malakahh <malakahh@users.noreply.github.com>
2017-09-26 23:03:34 +0200

    did some routing tests

--- Dolomite/Dolomite/Controllers/HomeController.cs
+++ Dolomite/Dolomite/Controllers/HomeController.cs
@@ -33,5 +33,12 @@ namespace Dolomite.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult NewTest()
+        {
+            ViewData["Message"] = "TEEEEEEEEEEEEEEEEEEEST";
+
+            return View();
+        }
     }
 }
