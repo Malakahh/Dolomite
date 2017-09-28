deddfc4b6a2a625680273f566b18315142156cb6
Malakahh <malakahh@users.noreply.github.com>
2017-09-28 20:49:19 +0200

    created API controller class

---
+++ Dolomite/Dolomite/Controllers/ApiController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Dolomite.Controllers
+{
+    public class ApiController : Controller
+    {
+        [HttpGet("api/test/{poop}")]
+        public IActionResult Test(int poop)
+        {
+            return new ObjectResult("This is a test" + poop);
+        }
+    }
+}
